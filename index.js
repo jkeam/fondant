a79ca5d2b0955e1d4e3880b2d6c4f2b586c38288
Jon Keam <jpkeam@gmail.com>
2021-02-01 15:20:39 -0500

    add search

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const fsp = require('fs').promises;
 const { authorize } = require('./lib/auth');
 const { read } = require('./lib/sheet');
-const { db, createCollection } = require('./lib/db');
+const { db, createDatabase, search } = require('./lib/db');
 
 require('dotenv').config()
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID || '';
 const RANGE = process.env.RANGE || '';
+const SEARCH_FIELD = process.env.SEARCH_FIELD || '';
 const CREDENTIALS_PATH = process.env.CREDENTIALS_PATH || 'credentials.json';
 const TOKEN_PATH = process.env.TOKEN_PATH || 'token.json';
+const JSON_PATH = process.env.JSON_PATH || 'database.json';
 const scope = process.env.READ_ONLY_SCOPE || 'https://www.googleapis.com/auth/spreadsheets.readonly';
 const SCOPES = [scope];   // If modifying these scopes, delete token.json.
 
 (async () => {
   try {
-    /**
-     *  Debug function
-     *  @params {string} name Title of the data dumped
-     *  @params {Array} models Models to print out
-     */
-    const dump = (name, models) => {
-      console.log(name);
-      console.log('----------------');
-      for (const model of models) {
-        console.log(model);
-      }
-    };
 
     // validate
     if (SPREADSHEET_ID === '') {
@@ -37,25 +27,43 @@ const SCOPES = [scope];   // If modifying these scopes, delete token.json.
     }
 
     /**
-     *  Destructively recreate database.
+     *  Debug function
+     *  @param {string} name Title of the data dumped
+     *  @param {Array} models Models to print out
      */
-    const createDatabase = async () => {
-      const content = await fsp.readFile(CREDENTIALS_PATH);
-      const authClient = await authorize(SCOPES, TOKEN_PATH, JSON.parse(content));
-      const sheets = await read(SPREADSHEET_ID, authClient, RANGE.split(',').map(i => i.trim()));
-
-      await db.drop();
-      for (const sheet of sheets) {
-        const { headers, originalHeaders, rows, models, name } = sheet;
-        const collection = await createCollection(name, headers, { force: true });
-        for (const model of models) {
-          await collection.create(model);
-        }
+    const dump = (name, models) => {
+      console.log(name);
+      console.log('----------------');
+      for (const model of models) {
+        console.log(model);
       }
     };
 
+    /**
+     *  Destructively recreate database.
+     */
+    const createDatastore = async () => {
+      const content = await fsp.readFile(CREDENTIALS_PATH);
+      const authClient = await authorize(SCOPES, TOKEN_PATH, JSON.parse(content));
+      const sheets = await read(SPREADSHEET_ID, authClient, RANGE.split(',').map(i => i.trim()));
+      return await createDatabase(authClient, sheets, JSON_PATH);
+    };
+
+    /**
+     *  Find matching rows.
+     *
+     *  @param {string} term Search term
+     *  @returns {Object[]} Return matching rows
+     */
+    const find = async (term) => {
+      const sheets = JSON.parse(await fsp.readFile(JSON_PATH));
+      return search(sheets, SEARCH_FIELD, term);
+    };
+
     // main
-    await createDatabase();
+    // await createDatastore();
+    const results = await find('ansible');
+    console.log(results);
   } catch (e) {
     console.error(e);
   }
